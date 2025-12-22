0d248b2e4fd873e4ab71df0faad8bbad69a567c5
Geovanejn <166084515+Geovanejn@users.noreply.github.com>
2025-12-22 00:29:02 -0300

    Revert "Change driver to dialect for PostgreSQL"
    This reverts commit 924c7afd9350254a352e2727669b7899b68f534b.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 const config: Config = {
   schema: "./shared/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },

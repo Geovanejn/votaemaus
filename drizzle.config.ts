75bd8d488a658949e862b4fddcffc91bfa39b46e
Geovanejn <166084515+Geovanejn@users.noreply.github.com>
2025-12-22 00:29:10 -0300

    Revert "Update schema path in drizzle configuration"
    This reverts commit eb8c18f21986fbb750bb2c92eb7b44f4c9263378.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./shared/schema/index.ts"
+  schema: "./shared/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {

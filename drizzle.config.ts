eb8c18f21986fbb750bb2c92eb7b44f4c9263378
Geovanejn <166084515+Geovanejn@users.noreply.github.com>
2025-12-21 23:17:25 -0300

    Update schema path in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./shared/schema.ts",
+  schema: "./shared/schema/index.ts"
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {

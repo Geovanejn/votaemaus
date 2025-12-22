924c7afd9350254a352e2727669b7899b68f534b
Geovanejn <166084515+Geovanejn@users.noreply.github.com>
2025-12-21 23:32:47 -0300

    Change driver to dialect for PostgreSQL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 const config: Config = {
   schema: "./shared/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },

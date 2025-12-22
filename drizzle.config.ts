412b0041d12e256092baeb2ffec268f0e1ddcc26
Geovanejn <166084515+Geovanejn@users.noreply.github.com>
2025-12-22 00:29:13 -0300

    Revert "Refactor drizzle config for schema and driver"
    This reverts commit 8ca6b8bd64be3d44e21dd138409e99a745e71a6a.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./shared/schema.ts",
+export default defineConfig({
+  schema: "./shared/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
